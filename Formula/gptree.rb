02542d9164f1965665847ebdad990095b518e06c
Travis V <github@travis.engineer>
2024-12-15 22:11:38 -0500

    Updated to version 1.0.2 - https://github.com/travisvn/gptree/commit/60ec893f5ebe95caaede8e32dd2e2a37c4964334

--- Formula/gptree.rb
+++ Formula/gptree.rb
@@ -1,15 +1,15 @@
 class Gptree < Formula
   desc "A CLI tool to provide LLM context for coding projects by combining project files into a single text file with directory tree structure."
   homepage "https://github.com/travisvn/gptree"
-  url "https://files.pythonhosted.org/packages/source/g/gptree-cli/gptree_cli-1.0.1.tar.gz"
-  sha256 "bedb17bcbbafcadd081249f972dcc12c8c77670eca41736aefcb0354b0d2773f"
+  url "https://files.pythonhosted.org/packages/source/g/gptree-cli/gptree_cli-1.0.2.tar.gz"
+  sha256 "9c0b679c0b15662687fe633e9574342242016af0ee925334d015205f03045f31"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
     # Use pip to install the package
-    system "python3", "-m", "pip", "install", "gptree-cli==1.0.1", "--prefix=#{prefix}"
+    system "python3", "-m", "pip", "install", "gptree-cli==1.0.2", "--prefix=#{prefix}"
   end
 
   test do

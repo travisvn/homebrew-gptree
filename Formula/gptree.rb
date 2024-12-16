1260929f5a16f83184010bb8f1c46042a5023929
Travis V <github@travis.engineer>
2024-12-16 01:57:02 -0500

    Update to 1.0.4 — fix for .gitignore and adding optional copy-to-clipboard flag / config

--- Formula/gptree.rb
+++ Formula/gptree.rb
@@ -1,15 +1,15 @@
 class Gptree < Formula
-  desc "A CLI tool to provide LLM context for coding projects by combining project files into a single text file with directory tree structure."
+  desc "A CLI tool to provide LLM context for coding projects by combining project files into a single text file (or clipboard text) with directory tree structure."
   homepage "https://github.com/travisvn/gptree"
-  url "https://files.pythonhosted.org/packages/source/g/gptree-cli/gptree_cli-1.0.2.tar.gz"
-  sha256 "9c0b679c0b15662687fe633e9574342242016af0ee925334d015205f03045f31"
+  url "https://files.pythonhosted.org/packages/source/g/gptree-cli/gptree_cli-1.0.4.tar.gz"
+  sha256 "a1819a15280f3cd49dfdce6561f4763769b225fde41b4314cee24b0935100bd3"
   license "MIT"
 
   depends_on "python@3.9"
 
   def install
     # Use pip to install the package
-    system "python3", "-m", "pip", "install", "gptree-cli==1.0.2", "--prefix=#{prefix}"
+    system "python3", "-m", "pip", "install", "gptree-cli==1.0.4", "--prefix=#{prefix}"
   end
 
   test do
